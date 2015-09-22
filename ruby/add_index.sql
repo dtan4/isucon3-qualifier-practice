9187890642235dce31deb3f519614da6145a8590
Daisuke Fujita <dtanshi45@gmail.com>
2015-09-22 19:03:42 +0900

    Recreate index [score: 4683.0]

--- ruby/add_index.sql
+++ ruby/add_index.sql
@@ -1,3 +1,4 @@
-create index memos_is_private_idx on memos(is_private);
-create index memos_user_idx on memos(user);
-create index memos_user_and_is_private_idx on memos(user, is_private);
+alter table `memos` add index `memos_is_private_idx` (`is_private`);
+alter table `memos` add index `index memos_user_idx` (`user`);
+alter table `memos` add index `memos_user_is_private_created_at_idx` (`user`, `is_private`, `created_at`);
+alter table `memos` add index `memos_is_private_created_at_id_idx` (`is_private`, `created_at`, `id`);
