521bfc025df1b1a69574baeb3ac632f8022c13be
Daisuke Fujita <dtanshi45@gmail.com>
2015-09-22 16:09:47 +0900

    Add index [score: 3683.5]

---
+++ ruby/add_index.sql
@@ -0,0 +1,2 @@
+create index memos_is_private_idx on memos(is_private);
+create index memos_user_idx on memos(user);
---
+++ ruby/init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mysql -u isucon isucon < /home/isucon/webapp/ruby/add_index.sql
